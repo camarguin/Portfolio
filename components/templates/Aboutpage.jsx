24f93729862fddd85b72d4652caae4b689dbfc8c
Lucas Gerhardt de Camargo <lucasgcamargo@icloud.com>
2021-09-08 11:25:23 -0700

    Changed resume link

--- components/templates/Aboutpage.jsx
+++ components/templates/Aboutpage.jsx
@@ -33,7 +33,7 @@ const Aboutpage = () => {
           </a>
         </IconsContainer>
         <ButtonsContainer isDouble>
-          <MyButton btnLabel="Resume" btnHref="https://drive.google.com/file/d/1uX9KeFbUMPvAY2s0FEXhUuRvrbzam2Rw/view?usp=sharing" />
+          <MyButton btnLabel="Resume" btnHref="https://drive.google.com/file/d/1JI2qGIDUeYrDHCeDfcN-YId5-kbyVEr1/view?usp=sharing" />
           <MyButton btnLabel="Contact me" btnHref="mailto:lucasgcamargo@icloud.com" />
         </ButtonsContainer>
       </LeftContainer>
